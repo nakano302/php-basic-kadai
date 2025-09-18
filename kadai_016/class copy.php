1f23f26f270672063b6e13f25ed8441a05ebeea4
nakano302 <yukko_o302@yahoo.co.jp>
2025-09-19 00:07:08 +0900

    kadai_016 upload

---
+++ kadai_016/class copy.php
@@ -0,0 +1,22 @@
+<?php
+class Food {
+  public $name;
+  public $price;
+
+public function show_price(strig $price){
+$this->price = $price;
+}
+}
+
+class Animal {
+  public $name;
+  public $height;
+  public $weight;
+
+  $animal = new Animal();
+
+public function show_height(strig $height){
+$this->height = $height;
+}
+}
+?>
